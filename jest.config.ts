afbb12e5d3b232444da29cc653c62b9f07aeec81
Lucas Ribeiro <lucas@aorahub.com.br>
2024-01-21 15:51:33 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  roots: ['<rootDir>/src']
+}
+
+export default config
